f73a2efffb8a2449b0a5b59f1a310d73b6cb3cbd
murphbra <murphbra@oregonstate.edu>
2021-05-24 00:33:53 -0600

    Update homeworkFive.js

--- murphbra/homeworkFive.js
+++ murphbra/homeworkFive.js
@@ -1,38 +1,29 @@
 var express = require('express');
+
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-var bodyParser = require('body-parser');
-
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 5461);
 
-app.get('/', function(req,res){
-    res.render("get-page")
-})
-
-//app.post('/', function(req,res){
-    //res.render("post-page")
-//})
+app.get('/',function(req,res){
+  res.render('get-page.handlebars') //We can omit the .handlebars extension as we do below
+});
 
 
 app.use(function(req,res){
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-  });
-  
-  app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.type('plain/text');
-    res.status(500);
-    res.send('500 - Server Error');
-  });
+  res.status(404);
+  res.render('404');
+});
 
-  app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-  });
-  
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
