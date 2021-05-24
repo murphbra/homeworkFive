e50082a73a3018f12f88454ac46f19c7104433a7
murphbra <murphbra@oregonstate.edu>
2021-05-23 23:33:26 -0600

    commit 2

---
+++ murphbra/homeworkFive.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5461);
+
+app.get('/', function(req,res){
+    res.render("get-page")
+})
+
+//app.post('/', function(req,res){
+    //res.render("post-page")
+//})
+
+
+app.use(function(req,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+  });
+  
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.send('500 - Server Error');
+  });
+
+  app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  });
+  
